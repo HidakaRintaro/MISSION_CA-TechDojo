f1987ef34711951844c332abd7b33af4e0c60052
日高凜太郎 <hidakarintarou@hidakarintarounoMacBook-Pro.local>
2021-02-21 22:11:00 +0900

    ガチャの実装 ユーザーキャラクターリストの実装 tokenのランダム生成の追加

--- app/main.go
+++ app/main.go
@@ -9,8 +9,6 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"sort"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -49,6 +47,16 @@ func DbInfo() (db *sql.DB) {
 	return db
 }
 
+func Random() string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, 10)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
+
 // ==================== User ====================
 
 type UserCreateRequest struct {
@@ -62,7 +70,7 @@ func UserCreate(w http.ResponseWriter, r *http.Request) {
 	var reqUser UserCreateRequest
 
 	json.Unmarshal(StreamToByte(r.Body), &reqUser)
-	token := "aaabbbcc"
+	token := Random()
 	resUser := UserCreateResponse{
 		Token: token,
 	}
@@ -123,66 +131,81 @@ type GachaDrawRequest struct {
 	Times int `json:"times"`
 }
 type GachaResult struct {
-	CharacterID string `json:"characterID"`
+	CharacterID int    `json:"characterID,string"`
 	Name        string `json:"name"`
 }
 type GachaDrawResponse struct {
 	Results []GachaResult
 }
 
-type Weight struct {
-	CharacterId int
-	Rate int
-}
 func GachaDraw(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("x-token")
+	var weight = []int{
+		5,  // S
+		15, // A
+		30, // B
+		50, // C
+	}
+	var rarity = []string{"S", "A", "B", "C"}
+	var rank string
 
 	var reqGacha GachaDrawRequest
 	json.Unmarshal(StreamToByte(r.Body), &reqGacha)
 
 	/* キャラクターの取得 */
 	// DBから指定ガチャのキャラの排出率を取得
-	gachaId := 1
-	var weights []Weight
-	var weight Weight
+	gachaId := 3 // 今回はガチャの種類を固定値とする
+	var gacharesult GachaResult
+	var rankCharacter []GachaResult // ガチャのランクで取得できたキャラの一時保管
+	var result []GachaResult        // ガチャ結果
 	db := DbInfo()
 	defer db.Close()
-	row := db.QueryRow("SELECT character_id, rate FROM emission WHERE gacha_id = ?", gachaId)
-	for row.Next() {
-		row.Scan(&weight.CharacterId, &weight.Rate)
-		weights = append(weights, weight)
+
+	for time := 0; time < reqGacha.Times; time++ {
+
+		boundaries := make([]int, len(weight)+1)
+		for i := 1; i < len(weight)+1; i++ {
+			boundaries[i] = boundaries[i-1] + weight[i-1]
+		}
+		boundaries = boundaries[1:len(boundaries)] // 頭の0値は不要なので破棄
+
+		draw := rand.Intn(boundaries[len(boundaries)-1])
+
+		for i, boundary := range boundaries {
+			if draw <= boundary {
+				rank = rarity[i]
+				break
+			}
+		}
+		rows, _ := db.Query("SELECT e.character_id, c.name FROM emission e JOIN `character` c ON c.id = e.character_id WHERE e.gacha_id = ? AND c.rarity = ?",
+			gachaId,
+			rank,
+		)
+		cnt := 0
+		for rows.Next() {
+			rows.Scan(&gacharesult.CharacterID, &gacharesult.Name)
+			rankCharacter = append(rankCharacter, gacharesult)
+			cnt++
+		}
+		result = append(result, rankCharacter[rand.Intn(cnt)])
 	}
 
+	var resGacha GachaDrawResponse
+	resGacha.Results = result
 
+	/* tokenからuseridの取得 */
+	var id int
+	db.QueryRow("SELECT id FROM user WHERE `x-token` = ?", token).Scan(&id)
 
 	/* キャラクターの保存 */
-
-}
-// 重み付き乱数
-func CharacterGet(Weights []int) {
-	sort.Ints(Weights) // 重みの昇順でチェックしていくので重み一覧をソートする
-
-	// 抽選結果チェックの基準となる境界値を生成
-	boundaries := make([]int, len(Weights)+1) // intのスライスを作成(重み一覧より１大きい容量)
-
-	for i := 1; i < len(Weights)+1; i++ {
-		boundaries[i] = boundaries[i-1] + Weights[i-1]
+	for _, character := range result {
+		stmtInsert, _ := db.Prepare("INSERT INTO possession (`character_id`, `user_id`) VALUES(?, ?)")
+		stmtInsert.Exec(character.CharacterID, id)
+		stmtInsert.Close()
 	}
-	boundaries = boundaries[1:len(boundaries)] // 頭の0値は不要なので破棄
 
-	rand.Seed(time.Now().UnixNano())
-	result := make([]int, len(Weights))
-
-	draw := rand.Intn(boundaries[len(boundaries)-1]) // + 1
-
-	for i, boundary := range boundaries {
-
-		if draw <= boundary {
-			result[i]++
-			break
-
-		}
-	}
+	b, _ := json.Marshal(resGacha)
+	w.Write(b)
 }
 
 // ==================== Character ====================
@@ -202,15 +225,19 @@ func CharacterList(w http.ResponseWriter, r *http.Request) {
 	db := DbInfo()
 	defer db.Close()
 
-	var resUser CharacterListResponse
+	var userCharacter UserCharacter
+	var resList CharacterListResponse
 
-	row := db.QueryRow(
-		"SELECT  FROM `possession` p JOIN `user` u ON u.id = p.user_id WHERE u.x-token = ?",
+	rows, _ := db.Query(
+		"SELECT p.id AS userCharacterID, c.id AS characterID, c.name FROM `possession` p JOIN `user` u ON u.id = p.user_id JOIN `character` c ON c.id = p.character_id WHERE u.`x-token` = ?",
 		token,
 	)
 
-	row.Scan(&resUser)
+	for rows.Next() {
+		rows.Scan(&userCharacter.UserCharacterID, &userCharacter.CharacterID, &userCharacter.Name)
+		resList.Characters = append(resList.Characters, userCharacter)
+	}
 
-	b, _ := json.Marshal(resUser)
+	b, _ := json.Marshal(resList)
 	w.Write(b)
 }
